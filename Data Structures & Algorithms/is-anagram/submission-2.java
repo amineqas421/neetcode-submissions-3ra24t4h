7f2d9ff1dc2f6463869731bf91da4669a26d24ca
amineqas421 <37209784+amineqas421@users.noreply.github.com>
2026-07-13 16:28:10 -0400

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length())return false;
+        char[] sortS = s.toCharArray();
+        char[] sortT = t.toCharArray();
+        Arrays.sort(sortS);
+        Arrays.sort(sortT);
+        return Arrays.equals(sortS, sortT);
+
+    }
+}
